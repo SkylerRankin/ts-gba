d3627d84df5b09b0ad2a78fcc68045b4914228b8
skylerrankin <skylerrankin@gmail.com>
2024-03-15 15:34:30 -0400

    Add ui for palettes, tiles, and display control.

--- src/emulator/ui.ts
+++ src/emulator/ui.ts
@@ -62,10 +62,109 @@ const getFrameInfo = (gba: GBA) => {
     return { fps, frameTime, cycles };
 }
 
+const getPaletteColors = (gba: GBA) => {
+    const bytesPerColor = 2;
+
+    const bgPalette: string[] = [];
+    const bgPaletteStartAddress = 0x05000000;
+    for (let i = 0; i < 16 * 16; i++) {
+        const color = get15BitColorFromAddress(gba, bgPaletteStartAddress + i * bytesPerColor);
+        bgPalette.push(
+            `rgb(` +
+            `${Math.floor(color.red)}, ` +
+            `${Math.floor(color.green)}, ` +
+            `${Math.floor(color.blue)}` +
+            `)`
+        );
+    }
+
+    const spritePalette: string[] = [];
+    const spritePaletteStartAddress = 0x05000200;
+    for (let i = 0; i < 16 * 16; i++) {
+        const color = get15BitColorFromAddress(gba, spritePaletteStartAddress + i * bytesPerColor);
+        spritePalette.push(
+            `rgb(` +
+            `${Math.floor(color.red)}, ` +
+            `${Math.floor(color.green)}, ` +
+            `${Math.floor(color.blue)}` +
+            `)`
+        );
+    }
+
+    return { bgPalette, spritePalette };
+}
+
+const getTiles = (gba: GBA, charblockIndex: number, paletteMode16: boolean, paletteIndex: number) => {
+    const bgPaletteStartAddress = charblockIndex < 4 ? 0x05000000 : 0x05000200;
+    const vramStartAddress = 0x06000000;
+    const charblockSize = 0x4000;
+    const tilesPerCharblock = paletteMode16 ? 512 : 256;
+    const bytesPerTile = paletteMode16 ? 32 : 64;
+    const bytesPerPalette = 32;
+
+    const tiles: any[] = [];
+    for (let tileIndex = 0; tileIndex < tilesPerCharblock; tileIndex++) {
+        const tileAddress = vramStartAddress + charblockIndex * charblockSize;
+        const tileBytes = gba.memory.getBytes(tileAddress + tileIndex * bytesPerTile, bytesPerTile);
+        const tile: any[] = [];
+        for (let i = 0; i < 64; i++) {
+            if (paletteMode16) {
+                const colorIndex = i % 2 === 0 ?
+                    tileBytes[i / 2] & 0xF :
+                    (tileBytes[Math.floor(i / 2)] >> 4) & 0xF;
+                const color = get15BitColorFromAddress(gba, bgPaletteStartAddress + paletteIndex * bytesPerPalette + colorIndex * 2);
+                tile.push(color);
+            } else {
+                const colorIndex = tileBytes[i] & 0xFF;
+                const color = get15BitColorFromAddress(gba, bgPaletteStartAddress + colorIndex * 2);
+                tile.push(color);
+            }
+        }
+        tiles.push(tile); 
+    }
+
+    return tiles;
+}
+
+const getDisplayControl = (gba: GBA) => {
+    const displayControlAddress = 0x04000000;
+    const displayControl = gba.memory.getInt16(displayControlAddress).value;
+
+    let backgrounds = "";
+    for (let i = 8; i <= 11; i++) {
+        if (((displayControl >> i) & 0x1) === 1) {
+            if (backgrounds.length > 0) backgrounds += ", ";
+            backgrounds += `${i - 8}`;
+        }
+    }
+
+    return {
+        displayMode: displayControl & 0x7,
+        frameSelect: (displayControl >> 4) & 0x1,
+        hblankIntervalFree: (displayControl >> 5) & 0x1,
+        objVRAMMapping: ((displayControl >> 6) & 0x1) === 1 ? "1d" : "2d",
+        forcedBlank: (displayControl >> 7) & 0x1,
+        enabledBackgrounds: backgrounds,
+        displayObj: ((displayControl >> 12) & 0x1) === 1 ? "On" : "Off",
+    };
+}
+
+const get15BitColorFromAddress = (gba: GBA, address: number) : any => {
+    const colorData = gba.memory.getInt16(address).value;
+    return {
+        red: 255 * ((colorData >>> 0) & 0x1F) / 32,
+        green: 255 * ((colorData >>> 5) & 0x1F) / 32,
+        blue: 255 * ((colorData >>> 10) & 0x1F) / 32,
+    };
+}
+
 const UI = {
     getInstructionTableLines,
     getRegisterText,
-    getFrameInfo
+    getFrameInfo,
+    getPaletteColors,
+    getTiles,
+    getDisplayControl,
 };
 
 export { UI }
