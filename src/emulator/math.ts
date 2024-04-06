47698ffc798b8562505ce5f1406d5c0f16b25835
skylerrankin <skylerrankin@gmail.com>
2024-04-06 16:35:15 -0400

    Delete unused files, remove Long dependency.

--- src/emulator/math.ts
+++ src/emulator/math.ts
@@ -1,5 +1,3 @@
-import Long from 'long';
-
 const rotateRight = (n: number, r: number, bits: number) : number => {
     return ((n >>> r) | (n << (bits - r))) & (Math.pow(2, bits) - 1);
 }
@@ -29,44 +27,6 @@ const logicalShiftLeft = (n: number, s: number) : number[] => {
     if (s === 32) return [0, n & 0x1];
     if (s === 0) return [n, 0];
     return [n << s >>> 0, (n >>> (32 - s)) & 0x1];
-}
-
-// Converts a number n into values r and i such that n = i >> 2r. This operation (>>) is a right rotation.
-// The bitwise sizes of n, r, and i are restricted by bits, rotBits, and immBits respectively.
-// Returns an array [rotation, immediate].
-function encodeWithRotation(n: any | number | string | Long, bits:number, rotBits: number, immBits: number) : number[] {
-    n = (typeof(n) === 'string') ? Long.fromString(n, true, 16) : 
-        (typeof(n) === 'number') ? Long.fromNumber(n) :
-        n;
-
-    if (n.greaterThanOrEqual(0) && n.lessThanOrEqual(Math.pow(immBits, 2) - 1)) return [0, n.toInt()];
-    let r = 0;
-    let max: Long = new Long(Math.pow(2, immBits) - 1);
-
-    while ((n.lessThan(Long.ZERO) || n.greaterThan(max)) && r < bits) {
-        n = n.shiftLeft(1).or(n.shiftRightUnsigned(bits - 1)).and(Math.pow(2, bits) - 1);
-        r++;
-    }
-
-    // If the immediate was shifted into the correct range, but used an odd number of rotated bits,
-    // then shift one more to remain in the range, but use an even number of rotated bits. Only works
-    // if the most significant bit is a zero.
-    if (n.greaterThanOrEqual(Long.ZERO) && n.lessThanOrEqual(max) && r % 2 !== 0) {
-        if (n.lessThanOrEqual(max.divide(2))) {
-            r++;
-            n = n.shiftLeft(1);
-        }
-    }
-
-    if (r % 2 !== 0) {
-        throw new Error('Rotation must be even.');
-    } else if (n.greaterThanOrEqual(Long.fromInt(Math.pow(2, immBits)))) {
-        throw new Error(`Required immediate is too wide.`);
-    } else if (r / 2 > Math.pow(2, rotBits)) {
-        throw new Error('Required rotation is too large.');
-    }
-
-    return [r / 2, n.toInt()];
 }
 
 // Sign extends a number n in `bits` bits to the same number in 32 bits.
@@ -214,7 +174,7 @@ const halfWordAlignAddress = (address: number) : number => {
     return address & 0xFFFFFFFE;
 }
 
-export { encodeWithRotation, rotateRight, rotateLeft, logicalShiftRight,
+export { rotateRight, rotateLeft, logicalShiftRight,
     arithmeticShiftRight, logicalShiftLeft, signExtend, byteArrayToInt32,
     int32ToByteArray, int16ToByteArray, int8ToByteArray, numberOfSetBits,
     asHex, toBigEndianInt32, toBigEndianInt16, isNegative32, borrowFrom,
--- test/emulator/math.test.ts
+++ test/emulator/math.test.ts
@@ -1,5 +1,5 @@
 import {
-    encodeWithRotation, rotateRight, rotateLeft, signExtend,
+    rotateRight, rotateLeft, signExtend,
     byteArrayToInt32, int32ToByteArray, toBigEndianInt32, toBigEndianInt16, logicalShiftLeft, borrowFrom, twosComplementNegation, int16ToByteArray, int8ToByteArray, isNegative32 } from '../../src/emulator/math';
 
 test('logicalShiftLeft', () => {
@@ -22,18 +22,6 @@ test('rotateLeft', () => {
     expect(rotateLeft(9, 0, 8)).toBe(9);
     expect(rotateLeft(9, 1, 8)).toBe(18);
     expect(rotateLeft(73, 5, 7)).toBe(50);
-});
-
-test('encodeWithRotation', () => {
-    expect(() => encodeWithRotation(225, 8, 4, 4)).toThrow('Rotation must be even.');
-    expect(() => encodeWithRotation(258, 32, 4, 8)).toThrow('Rotation must be even.');
-    expect(() => encodeWithRotation('0xFF0000FF', 32, 4, 8)).toThrow('Required immediate is too wide.');
-    expect(encodeWithRotation('0xC0000034', 32, 4, 8)).toStrictEqual([1, 211]);
-    expect(encodeWithRotation(261120, 32, 4, 8)).toStrictEqual([11, 255]);
-    expect(encodeWithRotation('0x0A000000', 32, 4, 8)).toStrictEqual([4, 10]);
-    expect(encodeWithRotation('0x0A10000', 32, 4, 8)).toStrictEqual([8, 161]);
-    expect(encodeWithRotation('0xAF0', 32, 4, 8)).toStrictEqual([14, 175]);
-    expect(encodeWithRotation('0xC0000001', 32, 4, 8)).toStrictEqual([1, 7]);
 });
 
 test('signExtend', () => {
